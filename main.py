fa301cd4d17671a67155b05086cf5870fbf03cf9
AlexandrVino <64733647+ded-masay@users.noreply.github.com>
2020-12-22 11:28:21 +0500

    grid in canvas

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import pygame
 class Canvas:
     def __init__(self):
         info = pygame.display.Info()
-        self.window_size = info.current_w, info.current_h - 60
+        self.window_size = info.current_w, info.current_h - 70
         self.curr_screen = 0
+        self.cell_sizes = [int((self.window_size[1] + 70) / 7), int(self.window_size[1] / 7)]
+        self.matrix = [[0] * 7 for _ in range(int(self.window_size[0] / 7))]
 
     def set_screen(self):
         self.curr_screen = not self.curr_screen
@@ -17,7 +19,11 @@ class Canvas:
             return pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
     def render(self, screen):
-        pass
+        cell_size = self.cell_sizes[self.curr_screen]
+        for x in range(len(self.matrix)):
+            for y in range(len(self.matrix[x])):
+                pygame.draw.rect(screen, 'white', (x * cell_size + 3, y * cell_size,
+                                                   cell_size, cell_size), 3)
 
 
 def main():
@@ -36,6 +42,7 @@ def main():
                 screen = canvas.set_screen()
 
         screen.fill((0, 0, 0))
+        canvas.render(screen)
         pygame.display.flip()
     pygame.quit()
 
