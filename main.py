8774e95b6ca4ad588fb051a26daced9a4f5f5d2a
AlexandrVino <64733647+ded-masay@users.noreply.github.com>
2020-12-22 17:32:40 +0500

    add player in project

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pygame
 
+import program.Entities as Entities
+import program.Weapons as Weapons
+import program.Enchantment as Enchantment
+
 
 class Canvas:
     def __init__(self):
@@ -7,7 +11,9 @@ class Canvas:
         self.window_size = info.current_w, info.current_h - 70
         self.curr_screen = 0
         self.cell_sizes = [int((self.window_size[1] + 70) / 7), int(self.window_size[1] / 7)]
-        self.matrix = [[0] * 7 for _ in range(int(self.window_size[0] / 7))]
+        self.matrix = [[0] * 15 for _ in range(7)]
+
+        self.left = 0
 
     def set_screen(self):
         self.curr_screen = not self.curr_screen
@@ -22,8 +28,11 @@ class Canvas:
         cell_size = self.cell_sizes[self.curr_screen]
         for x in range(len(self.matrix)):
             for y in range(len(self.matrix[x])):
-                pygame.draw.rect(screen, 'white', (x * cell_size + 3, y * cell_size,
+                pygame.draw.rect(screen, 'black', (y * cell_size + 3 + self.left, x * cell_size,
                                                    cell_size, cell_size), 3)
+
+    def change_left_padding(self, left):
+        self.left += left
 
 
 def main():
@@ -31,6 +40,9 @@ def main():
 
     canvas = Canvas()
     screen = canvas.set_screen()
+
+    player_sprite_group = pygame.sprite.Group()
+    player = Entities.Player([0, 0], 'Player', 100, player_sprite_group, canvas.window_size, canvas)
 
     running = True
     while running:
@@ -41,8 +53,13 @@ def main():
             if event.type == pygame.KEYUP and event.key == pygame.K_F11:
                 screen = canvas.set_screen()
 
-        screen.fill((0, 0, 0))
+            keys = pygame.key.get_pressed()
+            if keys:
+                player.move(keys)
+
+        screen.fill((200, 200, 200))
         canvas.render(screen)
+        player_sprite_group.draw(screen)
         pygame.display.flip()
     pygame.quit()
 
